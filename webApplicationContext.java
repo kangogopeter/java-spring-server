af2c6f5a820d37116d4bb71abac302c20edb45bb
kangogopeter <peterkangogo501@gmail.com>
2020-12-19 17:03:21 +0300

    settting up a dependecy

---
+++ webApplicationContext.java
@@ -0,0 +1,17 @@
+public class MyWebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class<?>[] { RootConfig.class };
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class<?>[] { App1Config.class };
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] { "/app1/*" };
+    }
+}
